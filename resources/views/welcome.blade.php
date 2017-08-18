18307d82baec07987a564a8ad946d244bcc37f1e
Armandocs16 <ep-armand@hotmail.com>
2017-08-18 00:40:00 -0500

    Se agrego la autentificacion auth usando el comando make:auth, este comando creo nuevas rutas, estas se encontraban el direcorio Routers.php en el metodo auth, se copiaron las rutas y se agregaron en el archivo web.php para tener una mejor legibilidad del codigo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Prueba Git
                 </div>
 
                 <div class="links">
